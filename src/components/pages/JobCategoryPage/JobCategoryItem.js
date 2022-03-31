df196917a39eabd3afb2ae51528568a3a1e42fe6
Ryan <AggroCake@gmail.com>
2022-03-31 15:18:53 +0800

    Update JobCategoryItem.js

--- src/components/pages/JobCategoryPage/JobCategoryItem.js
+++ src/components/pages/JobCategoryPage/JobCategoryItem.js
@@ -9,13 +9,20 @@ function JobCategoryItem({ category }) {
   // const [isDataFullyLoaded, setIsDataFullyLoaded] = useState(false)
 
   useEffect(() => {
-    getJobList(category.companyId, category.categoryId).then(({ data }) =>
-      setData(data)
-    )
+    getJobList(category.companyId, category.categoryId).then(({ data }) => setData(data))
   }, [category])
 
   const handleClick = () => {
     setCollapsed((prevValue) => !prevValue)
+  }
+
+  const getTotalCount = () => {
+    if (!data || !data.list) return 0
+
+    let count = 0
+    if (data.list.topJobs) count += data.list.topJobs.length
+    if (data.list.normalJobs) count += data.list.normalJobs.length
+    return count
   }
 
   return (
@@ -29,14 +36,27 @@ function JobCategoryItem({ category }) {
         >
           {category.text}
         </a>
-        {data && data.totalCount}
+        {getTotalCount()}
       </div>
-      {!collapsed && data && data.list.normalJobs && (
-        <div>
-          {data.list.normalJobs.map((job, idx) => (
-            <JobEntry key={job.jobNo} job={job} idx={idx} />
-          ))}
-        </div>
+      {!collapsed && data && (
+        <>
+          {data.list.topJobs && (
+            <div>
+              <h3>Pinned jobs</h3>
+              {data.list.topJobs.map((job, idx) => (
+                <JobEntry key={job.jobNo} job={job} idx={idx} />
+              ))}
+            </div>
+          )}
+          {data.list.normalJobs && (
+            <div>
+              <h3>Normal jobs</h3>
+              {data.list.normalJobs.map((job, idx) => (
+                <JobEntry key={job.jobNo} job={job} idx={idx} />
+              ))}
+            </div>
+          )}
+        </>
       )}
     </div>
   )
